9aa41a48d7eb36496424b492abd071f324e3ff90
Peter Lamberg <pgithub@pelam.fi>
2017-04-02 11:19:11 +0300

    Refactor SDL test.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,13 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated"
+
 #include "SDL2/SDL.h" // include SDL header
 #include "stdint.h"
 #include "stdlib.h"
 
-using namespace std;
+#pragma clang diagnostic pop
+
+//using namespace std;
 
 struct particle
 {
@@ -12,58 +17,118 @@ struct particle
     uint16_t yVel; // 8 fractional bits
 };
 
-int const particleCount = 10000;
+typedef uint8_t DensityBufferType;
 
-// http://wiki.libsdl.org/SDL_GetWindowSurface?highlight=%28%5CbCategoryVideo%5Cb%29%7C%28CategoryEnum%29%7C%28CategoryStruct%29
-// http://stackoverflow.com/questions/20579658/pixel-drawing-in-sdl2-0#
-int main(int argc, char* argv[])
-{
-    unsigned char* densityBuffer = (unsigned char*)malloc(640 * 480);
-    particle* particleBuffer = (particle*)malloc(particleCount * sizeof(particle));
-    SDL_Event event;    
+
+static int const screenWidth = 640;
+static int const screenHeight = 480;
+static int const densityBufferWidth = screenWidth;
+static int const densityBufferHeight = screenHeight;
+static int const particleCount = 10000;
+static int const particleBufferSize = particleCount * sizeof(particle);
+static int const densityBufferSize = densityBufferWidth * densityBufferHeight * sizeof(DensityBufferType);
+static DensityBufferType* densityBuffer = 0;
+static particle* particleBuffer = 0;
+static SDL_Event event;
+static SDL_Window* window = 0;
+static SDL_Surface* screen = 0;
+static bool exitRequested = false;
+static int bpp = 0;
+static int frameCounter = 0;
+
+
+static void allocateBuffers() {
+    densityBuffer = (DensityBufferType*)malloc(densityBufferSize);
+    memset(densityBuffer, 0, densityBufferSize);
+
+    particleBuffer = (particle*)malloc(particleBufferSize);
+    memset(particleBuffer, 0, particleBufferSize);
+}
+
+static DensityBufferType* densityAddr(int x, int y) {
+    return &densityBuffer[y * densityBufferWidth + x];
+}
+
+static void densityBlock(int x, int y, int w, int h, DensityBufferType const& v) {
+    for(int xp = x; xp < x + w; xp++) {
+        for(int yp = y; yp < y + h; yp++) {
+            *densityAddr(xp, yp) = v;
+        }
+    }
+}
+
+static void drawInitialDensityMap() {
+    DensityBufferType const densityHard = 250;
+    densityBlock(0, densityBufferHeight - 10, densityBufferWidth, 10, densityHard); // bottom
+    densityBlock(0, 0, 10, densityBufferHeight, densityHard); // left
+    densityBlock(densityBufferWidth - 10, 0, densityBufferHeight, 10, densityHard); // right
+    densityBlock(densityBufferWidth / 2 - 10, densityBufferHeight - 30, 20, 20, densityHard); // center
+}
+
+static void sdlInit() {
+    // http://wiki.libsdl.org/SDL_GetWindowSurface?highlight=%28%5CbCategoryVideo%5Cb%29%7C%28CategoryEnum%29%7C%28CategoryStruct%29
+    // http://stackoverflow.com/questions/20579658/pixel-drawing-in-sdl2-0#
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
-
+    window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
     // instead of creating a renderer, we can draw directly to the screen
-    SDL_Surface* screen = SDL_GetWindowSurface(window);
+    screen = SDL_GetWindowSurface(window);
+    bpp = screen->format->BytesPerPixel;
+}
 
-    int bpp = screen->format->BytesPerPixel;
+static void sdlCleanup() {
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
-    int loop = 0;
+static void sdlHandleEvents() {
+    while(SDL_PollEvent(&event)) {
+        switch(event.type) {
+            case SDL_QUIT:
+            exitRequested = 1;
+            break;
+        }
+    }
+}
 
-    while (1)
+static void renderFrame() {
+    for (int x = 0; x < screenWidth; x++)
     {
-
-        if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
-
-        for (int x = 0; x < 640; x++)
+        for (int y = 0; y < screenHeight; y++)
         {
-            for (int y = 0; y < 480; y++)
+            for (int c = 0; c < bpp; c++)
             {
-                for (int c = 0; c < bpp; c++)
-                {
-                    Uint8* p = (Uint8*)screen->pixels + y * screen->pitch + x * bpp + c;
-                    *p = (((loop * c) + x) * (y + (loop * (c ^ 2)))) & 0xff;
-                }
+                Uint8* p = (Uint8*)screen->pixels + y * screen->pitch + x * bpp + c;
+                *p = (((frameCounter * c) + x) * (y + (frameCounter * (c ^ 2)))) & 0xff;
             }
         }
+    }
+}
 
+static void renderLoop() {
+    while (!exitRequested)
+    {
+        if (SDL_MUSTLOCK(screen)) SDL_LockSurface(screen);
+
+        renderFrame();
+        
         if (SDL_MUSTLOCK(screen)) SDL_UnlockSurface(screen);
 
         // this works just like SDL_Flip() in SDL 1.2
         SDL_UpdateWindowSurface(window);
+        frameCounter++;
 
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-        {
-            break;
-        }
-
-        loop++;
+        sdlHandleEvents();
     }
+}
 
-    // SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+int main(int argc, char* argv[])
+{
+    allocateBuffers();
+    drawInitialDensityMap();
+    sdlInit();
 
+    renderLoop();
+
+    sdlCleanup();
     return EXIT_SUCCESS;
 }
