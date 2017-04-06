c2e69ba643688ffb1256ef2a682343e706b68afa
Peter Lamberg <peter.lamberg@futuremark.com>
2017-04-06 13:07:52 +0300

    Windows build tweaks.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -24,6 +24,7 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "string.h"
+#include "stdlib.h"
 #pragma clang diagnostic pop
 
 struct particle
@@ -374,7 +375,7 @@ static void renderLoop()
     }
 }
 
-int main(int argc, char* argv[])
+int SDL_main(int argc, char* argv[])
 {
     allocateBuffers();
     drawInitialDensityMap();
